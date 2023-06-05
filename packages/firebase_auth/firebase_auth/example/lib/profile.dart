1a3a82b03c4d591603ba064650a58190ad238383
flying-wizzy <julsen-baron@gmx.de>
2023-06-05 20:02:02 +0200

    cleaned profile page to show minimal reproducible example

--- packages/firebase_auth/firebase_auth/example/lib/profile.dart
+++ packages/firebase_auth/firebase_auth/example/lib/profile.dart
@@ -11,9 +11,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 import 'auth.dart';
 
-/// Displayed as a profile image if the user doesn't have one.
-const placeholderImage =
-    'https://upload.wikimedia.org/wikipedia/commons/c/cd/Portrait_Placeholder_Square.png';
 
 /// Profile page shows after sign in or registerationg
 class ProfilePage extends StatefulWidget {
@@ -28,19 +25,13 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   late User user;
   late TextEditingController controller;
-  final phoneController = TextEditingController();
+  final newMailController = TextEditingController();
 
-  String? photoURL;
-
-  bool showSaveButton = false;
   bool isLoading = false;
 
   @override
   void initState() {
     user = auth.currentUser!;
-    controller = TextEditingController(text: user.displayName);
-
-    controller.addListener(_onNameChanged);
 
     auth.userChanges().listen((event) {
       if (event != null && mounted) {
@@ -51,45 +42,31 @@ class _ProfilePageState extends State<ProfilePage> {
     });
 
     log(user.toString());
-
     super.initState();
   }
 
-  @override
-  void dispose() {
-    controller.removeListener(_onNameChanged);
 
-    super.dispose();
-  }
 
-  void setIsLoading() {
-    setState(() {
-      isLoading = !isLoading;
-    });
-  }
-
-  void _onNameChanged() {
-    setState(() {
-      if (controller.text == user.displayName || controller.text.isEmpty) {
-        showSaveButton = false;
-      } else {
-        showSaveButton = true;
-      }
-    });
-  }
-
-  /// Map User provider data into a list of Provider Ids.
-  List get userProviders => user.providerData.map((e) => e.providerId).toList();
 
   Future updateDisplayName() async {
-    await user.updateDisplayName(controller.text);
-
-    setState(() {
-      showSaveButton = false;
-    });
-
-    // ignore: use_build_context_synchronously
+    await user.updateDisplayName("Hello Firebase Team");
     ScaffoldSnackbar.of(context).show('Name updated');
+  }
+
+  Future<void> handleVerifyAndUpdateEmail() async {
+    try {
+      await FirebaseAuth.instance.currentUser
+          ?.verifyBeforeUpdateEmail(newMailController.text);
+    } on FirebaseAuthException catch (err) {
+      if (err.code == "requires-recent-login") {
+        final bool? reAuthenticated = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ReAuthPage()));
+        if (reAuthenticated ?? false) {
+           await handleVerifyAndUpdateEmail();
+        }
+      }
+    }
   }
 
   @override
@@ -106,226 +83,134 @@ class _ProfilePageState extends State<ProfilePage> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Stack(
-                        children: [
-                          CircleAvatar(
-                            maxRadius: 60,
-                            backgroundImage: NetworkImage(
-                              user.photoURL ?? placeholderImage,
-                            ),
-                          ),
-                          Positioned.directional(
-                            textDirection: Directionality.of(context),
-                            end: 0,
-                            bottom: 0,
-                            child: Material(
-                              clipBehavior: Clip.antiAlias,
-                              color: Theme.of(context).colorScheme.secondary,
-                              borderRadius: BorderRadius.circular(40),
-                              child: InkWell(
-                                onTap: () async {
-                                  final photoURL = await getPhotoURLFromUser();
-
-                                  if (photoURL != null) {
-                                    await user.updatePhotoURL(photoURL);
-                                  }
-                                },
-                                radius: 50,
-                                child: const SizedBox(
-                                  width: 35,
-                                  height: 35,
-                                  child: Icon(Icons.edit),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
                       const SizedBox(height: 10),
-                      TextField(
-                        textAlign: TextAlign.center,
-                        controller: controller,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          alignLabelWithHint: true,
-                          label: Center(
-                            child: Text(
-                              'Click to add a display name',
-                            ),
-                          ),
-                        ),
-                      ),
+                      Text(user.displayName ?? "no display name"),
                       Text(user.email ?? user.phoneNumber ?? 'User'),
                       const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (userProviders.contains('phone'))
-                            const Icon(Icons.phone),
-                          if (userProviders.contains('password'))
-                            const Icon(Icons.mail),
-                          if (userProviders.contains('google.com'))
-                            SizedBox(
-                              width: 24,
-                              child: Image.network(
-                                'https://upload.wikimedia.org/wikipedia/commons/0/09/IOS_Google_icon.png',
-                              ),
-                            ),
-                        ],
-                      ),
                       const SizedBox(height: 20),
-                      TextButton(
-                        onPressed: () {
-                          user.sendEmailVerification();
-                        },
-                        child: const Text('Verify Email'),
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          final a = await user.multiFactor.getEnrolledFactors();
-                          print(a);
-                        },
-                        child: const Text('Get enrolled factors'),
-                      ),
                       TextFormField(
-                        controller: phoneController,
+                        controller: newMailController,
                         decoration: const InputDecoration(
-                          icon: Icon(Icons.phone),
-                          hintText: '+33612345678',
-                          labelText: 'Phone number',
+                          labelText: "Enter new Mail",
                         ),
                       ),
-                      const SizedBox(height: 20),
                       TextButton(
-                        onPressed: () async {
-                          final session = await user.multiFactor.getSession();
-                          await auth.verifyPhoneNumber(
-                            multiFactorSession: session,
-                            phoneNumber: phoneController.text,
-                            verificationCompleted: (_) {},
-                            verificationFailed: print,
-                            codeSent: (
-                              String verificationId,
-                              int? resendToken,
-                            ) async {
-                              final smsCode = await getSmsCodeFromUser(context);
-
-                              if (smsCode != null) {
-                                // Create a PhoneAuthCredential with the code
-                                final credential = PhoneAuthProvider.credential(
-                                  verificationId: verificationId,
-                                  smsCode: smsCode,
-                                );
-
-                                try {
-                                  await user.multiFactor.enroll(
-                                    PhoneMultiFactorGenerator.getAssertion(
-                                      credential,
-                                    ),
-                                  );
-                                } on FirebaseAuthException catch (e) {
-                                  print(e.message);
-                                }
-                              }
-                            },
-                            codeAutoRetrievalTimeout: print,
-                          );
-                        },
-                        child: const Text('Verify Number For MFA'),
+                        onPressed: handleVerifyAndUpdateEmail, child: const Text('Verify and Update Email'),
                       ),
                       TextButton(
-                        onPressed: () async {
-                          try {
-                            final enrolledFactors =
-                                await user.multiFactor.getEnrolledFactors();
-
-                            await user.multiFactor.unenroll(
-                              factorUid: enrolledFactors.first.uid,
-                            );
-                            // Show snackbar
-                            ScaffoldSnackbar.of(context).show('MFA unenrolled');
-                          } catch (e) {
-                            print(e);
-                          }
-                        },
-                        child: const Text('Unenroll MFA'),
+                        onPressed: isLoading ? null : updateDisplayName,
+                        child: const Text('Update Display Name'),
                       ),
-                      const Divider(),
                       TextButton(
                         onPressed: _signOut,
                         child: const Text('Sign out'),
+                      ),
+                      TextButton(
+                        onPressed: ()=> FirebaseAuth.instance.currentUser?.delete(),
+                        child: const Text('Delete Account'),
                       ),
                     ],
                   ),
                 ),
               ),
             ),
-            Positioned.directional(
-              textDirection: Directionality.of(context),
-              end: 40,
-              top: 40,
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 200),
-                child: !showSaveButton
-                    ? SizedBox(key: UniqueKey())
-                    : TextButton(
-                        onPressed: isLoading ? null : updateDisplayName,
-                        child: const Text('Save changes'),
-                      ),
-              ),
-            )
           ],
         ),
       ),
     );
   }
 
-  Future<String?> getPhotoURLFromUser() async {
-    String? photoURL;
-
-    // Update the UI - wait for the user to enter the SMS code
-    await showDialog<String>(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('New image Url:'),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Update'),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                photoURL = null;
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-          ],
-          content: Container(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              onChanged: (value) {
-                photoURL = value;
-              },
-              textAlign: TextAlign.center,
-              autofocus: true,
-            ),
-          ),
-        );
-      },
-    );
-
-    return photoURL;
-  }
 
   /// Example code for sign out.
   Future<void> _signOut() async {
     await auth.signOut();
     await GoogleSignIn().signOut();
+  }
+}
+
+class ReAuthPage extends StatelessWidget {
+  const ReAuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: EmailReAuthSection(),
+    );
+  }
+}
+
+class EmailReAuthSection extends StatelessWidget {
+  EmailReAuthSection({Key? key}) : super(key: key);
+
+  final TextEditingController _mailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _mailController,
+            decoration: const InputDecoration(
+              labelText: 'Enter Mail',
+            ),
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(
+              labelText: 'Enter Password',
+            ),
+          ),
+          _LoginButton(
+              mailController: _mailController,
+              passwordController: _passwordController,
+              formKey: formKey),
+        ],
+      ),
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton({
+    Key? key,
+    required this.mailController,
+    required this.passwordController,
+    required this.formKey,
+  }) : super(key: key);
+
+  final TextEditingController mailController;
+  final TextEditingController passwordController;
+  final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        if (mailController.text.isNotEmpty &&
+            passwordController.text.isNotEmpty) {
+          if(formKey.currentState?.validate() ?? false) {
+            try {
+              final AuthCredential mailCredential = EmailAuthProvider
+                  .credential(
+                email: mailController.text,
+                password: passwordController.text,
+              );
+              await FirebaseAuth.instance.currentUser
+                  ?.reauthenticateWithCredential(mailCredential);
+              Navigator.pop(context);
+            } catch (e){
+              debugPrint(e.toString());
+            }
+
+          }
+        } else {
+        }
+      }, child: const Text('Re-Authenticate'),
+    );
   }
 }
